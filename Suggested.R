c5bd507bb3766270ac5390106693720f5acecf37
Syed Amjad Ali <amjad.ali.syed110@gmail.com>
2022-12-19 00:10:34 +0100

    Made the dataframe tidier, we are now ready to run models

--- Suggested.R
+++ Suggested.R
@@ -17,18 +17,18 @@ load("df_agg.RData")
 unique(df$started_at)
 count(df$started_at)
 dates <- as.Date(df$started_at, '%d/%m/%Y') #this did not work
-###########
+#
 yr <- year(dates)
 monyr <- as.yearmon(dates)
 lst <- lapply(list(dates, yr, monyr), function(x) #i am not sure what happened here
   transform(df, Count=ave(seq_along(x), x, FUN= length)))
 names(lst) <- paste0('newdf', seq_along(lst))
 list2env(lst, envir=.GlobalEnv)
-######
+#
 
 counts <- data.frame(table(as.Date(index(df))))#don't know what happened here
 
-######
+#
 df %>% group_by(year=year(started_at)) %>% count()
 df %>% group_by(year=year(started_at), month=month(started_at)) %>% count()
 
@@ -97,7 +97,7 @@ reg_result<-(lm(count~temperature+wind+precipitation,y))
 summary(reg_result)
 
 
-######################
+#
 #so lets fix things up
 #lets, come back and clean everything . 
 #first, read the data in two separate data frame.
@@ -108,6 +108,7 @@ summary(reg_result)
 
 
 #################
+
 df <-
   list.files(path="data/", pattern = ".csv", full.names = T) %>% #we used list files to detect all csv files
   map_df(~read_csv(.)) #we used map function to iterate over all csv files in our folder
@@ -141,7 +142,7 @@ y <- data.frame(matrix(ncol = 5, nrow = 365))
 x <- c("day", "count", "precipitation","wind","temperature")
 colnames(y) <- x
 y$day<-shananigan$Time
-y$day<-dmy(y$day)
+#y$day<-dmy(y$day)  apparently this does not work now
 y$count<-Day_wise_count$n
 y$precipitation<-shananigan$Precipitation
 y$wind<-shananigan$Wind
@@ -150,17 +151,23 @@ y$temperature<-shananigan$Temperature
 ggplot(y,aes(x=wind,y=count))+geom_point()
 
 summary(lm(count~temperature,y))
-weekdays_label <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+# not needed for now weekdays_label <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+#y$day <- weekdays(as.Date(y$day))
 
 y<-
-mutate(y,weekdays(y$day))
+mutate(y,"weekdays"=weekdays(as.Date(y$day))) #fantastic! nice! jhakas! ekdam jhakas!https://www.youtube.com/shorts/EhLZrhuyeeI
 
-y<- y %>% relocate(`weekdays(y$day)`,.after = day)
+y<- y %>% relocate(weekdays,.after = day)
 
-y<-mutate(y,"dummy")
-
-y$`"dummy"`<- #i am so sexy
-ifelse(y$`weekdays(y$day)`=="Saturday"|y$`weekdays(y$day)`=="Sunday",1,0) #here we create our beautiful dummy variable :)
+XYZ<- #i am so sexy with dummies  
+  ifelse(y$weekdays=="Saturday"|y$weekdays=="Sunday",1,0) #here we create our beautiful dummy variable :)
 
 
-summary(lm(count~precipitation+wind+temperature,data=y))
+y<-mutate(y,"dummy"=XYZ)
+y<- y %>% relocate(dummy,.after = weekdays)
+
+regressions<-
+summary(lm(count~precipitation+wind+temperature+dummy,data=y))
+
+## nice, from here we just follow along what we learnt in class
